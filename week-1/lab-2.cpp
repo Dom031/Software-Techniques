bbd9d6b70438459ae12b4ea80ca60042647a8033
Domingos <dn22aau@herts.ac.uk>
2024-10-15 10:21:56 +0100

    This is for the lab 2 exercises

--- week-1/lab-2.cpp
+++ week-1/lab-2.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <string>
+
+std::string phoneNumFormat (std::string phonenum, std::string separator){
+  std::string formated = "(" + phonenum.substr(0,5) + ") " + phonenum.substr(5,3) + separator + phonenum.substr(8);
+  return formated;
+}
+
 
 int main () {
-  std:: string phonenum;
+  std:: string phonenum, separator;
+  
   std::cout << "enter a phone number with 11 digits: ";
   std::cin >> phonenum;
-  
-  std::cout << "The formated phone number is:(" << phonenum.substr(0,5) << ") " << phonenum.substr(5,3) << " " << phonenum.substr(8) << std::endl;
+ 
+  std::cout << "enter a separator (space os '-' preferred): ";
+  std::cin >> separator;
 
+  std::cout << "The formated phone number is: " << phoneNumFormat(phonenum, separator) << std::endl;
+  return 0;
 }
