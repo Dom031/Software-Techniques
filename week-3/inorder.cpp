ab2f444b0b6cee4e464f870ad8e249e64f39d999
Domingos <dn22aau@herts.ac.uk>
2024-10-22 08:49:21 +0100

    Completed exercise 1

--- week-3/inorder.cpp
+++ week-3/inorder.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+int main(){
+  int a, b, c;
+  
+  std::cout << "Enter three numbers: ";
+  std::cin >> a >> b  >> c;
+
+  if (a > b && b > c){
+    std::cout << "In order (descending)" << std::endl;
+  } else if ( a < b && b < c) {
+    std::cout << "In order (ascending)" << std::endl;
+  } else {
+    std::cout << "Not in order" << std::endl;
+  }
+  
+
+
+  return 0;
+}
